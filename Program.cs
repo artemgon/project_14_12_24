0e84a1c159c3f77f4e65704cf275a1b016a720f7
artemgon <artemhoncharuk123@gmail.com>
2025-01-12 19:06:25 +0200

    Task 1 is done.

--- Program.cs
+++ Program.cs
@@ -4,16 +4,26 @@ namespace project_14_12_24
 {
     internal class Program
     {
+        public delegate void ShowMessage(string message);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                ShowMessage showMessage = new(DisplayMessage);
+                showMessage("Hello World!"); 
+                Console.WriteLine();
+                showMessage.Invoke("Hello World!");
+                Console.WriteLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+        }
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
