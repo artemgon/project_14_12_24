c130622c6b815ee2706b337e8689d546f7302373
artemgon <artemhoncharuk123@gmail.com>
2025-01-12 20:53:49 +0200

    Task 3 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,89 @@
 ﻿
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
 namespace project_14_12_24.Classes
 {
-    internal class Classes
+    public class CreditCard
     {
+        public string? CardNumber { get; set; }
+        public string? HolderSNP { get; set; }
+        public string? ExpirationDate { get; set; }
+        private string? PIN;
+        public string? CreditLimit { get; set; }
+        public float Balance { get; set; }
+        public string? Pin
+        {
+            private get { return PIN; }
+            set
+            {
+                if (value != null && value.Length == 4)
+                {
+                    PIN = value;
+                }
+                else
+                {
+                    throw new Exception("PIN must be 4 digits long");
+                }
+            }
+        }
+        public event Action<float>? Replenish;
+        public event Action<float>? Withdraw;
+        public event Action<string>? CreditUsageStart;
+        public event Predicate<int>? LimitReached;
+        public event Action<string>? ChangePin;
+        public CreditCard()
+        {
+            CardNumber = "";
+            HolderSNP = "";
+            ExpirationDate = "";
+            Pin = "";
+            CreditLimit = "";
+            Balance = 0;
+        }
+        public CreditCard(string cardNumber, string holderSNP, string expirationDate, string pin, string creditLimit, float balance)
+        {
+            CardNumber = cardNumber;
+            HolderSNP = holderSNP;
+            ExpirationDate = expirationDate;
+            Pin = pin;
+            CreditLimit = creditLimit;
+            Balance = balance;
+        }
+        public void ReplenishBalance(float amount)
+        {
+            Balance += amount;
+            Replenish?.Invoke(amount);
+        }
+        public void WithdrawBalance(float amount)
+        {
+            Balance -= amount;
+            Withdraw?.Invoke(amount);
+        }
+        public void StartCreditUsage(string message)
+        {
+            CreditUsageStart?.Invoke(message);
+        }
+        public bool IsLimitReached(int amount)
+        {
+            return LimitReached?.Invoke(amount) ?? false;
+        }
+        public void ChangePinCode(string newPin)
+        {
+            Pin = newPin;
+            ChangePin?.Invoke(newPin);
+        }
+        public void Display()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Credit card information:");
+            Console.WriteLine($"Card number: {CardNumber}");
+            Console.WriteLine($"Holder SNP: {HolderSNP}");
+            Console.WriteLine($"Expiration date: {ExpirationDate}");
+            Console.WriteLine($"PIN: {PIN}");
+            Console.WriteLine($"Credit limit: {CreditLimit}");
+            Console.WriteLine($"Balance: {Balance}");
+            Console.WriteLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,30 @@ namespace project_14_12_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                CreditCard creditCard = new("1234 5678 9012 3456", "John Doe", "12/24", "1234", "1000", 0);
+                creditCard.Display();
+                Console.WriteLine();
+                Console.WriteLine("Enter the amount to replenish the balance:");
+                int amount = int.Parse(Console.ReadLine() ?? "");
+                creditCard.ReplenishBalance(amount);
+                Console.WriteLine();
+                Console.WriteLine("Enter the amount to withdraw from the balance:");
+                amount = int.Parse(Console.ReadLine() ?? "");
+                creditCard.WithdrawBalance(amount);
+                Console.WriteLine();
+                Console.WriteLine("Enter the date of the start of credit usage:");
+                string date = Console.ReadLine() ?? "";
+                Console.WriteLine();
+                Console.WriteLine("Enter the amount to check if the limit is reached:");
+                amount = int.Parse(Console.ReadLine() ?? "");
+                Console.WriteLine(creditCard.IsLimitReached(amount));
+                Console.WriteLine();
+                Console.WriteLine("Enter the new PIN:");
+                string pin = Console.ReadLine() ?? "";
+                creditCard.ChangePinCode(pin);
+                Console.WriteLine();
+                creditCard.Display();
+                Console.WriteLine();
             }
             catch (Exception e)
             {
